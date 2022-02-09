a1b0eb5916365963f6c94ccf0b28cd499ede6b29
Oscar Fernandez <55814286+Evil004@users.noreply.github.com>
2022-02-09 15:30:53 +0100

    Create CrearUsuarios.sh

---
+++ Fase1/CrearUsuarios.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+while IFS=: read -r Name FirstName LastName account Dni Delegation Departament Computer Enabled Password TurnPassDays Change email Shell PathUser
+do
+        echo $Name
+	pathUO="$PathUser"
+        # Extraemos los campos de los usuarios
+        echo "Crando a $account en $pathUO"
+
+        samba-tool user create $account $Password --must-change-at-next-login --userou="$pathUO" --home-directory="/home/$account" --given-name="$Name" --surname="$FirstName $LastName" --mail="$email" --department="$Departament" --login-shell="$Shell"
+        #echo "samba-tool user create $account $Password --must-change-at-next-login --userou=$pathUO --home-directory=/home/$account --given-name=$Name --surname=$FirstName $LastName --mail=$email --departament=$Departament --login-shell=$Shell"
+
+
+
+        echo "\n"
+done <  $1
